d8a1e89b9a8ce35ec70bfa60358191509896d541
Alex Huang <alexh95@mit.edu>
2020-04-08 00:20:48 -0400

    Use config file, add initial user routes/db interface

--- authenticator/server.js
+++ authenticator/server.js
@@ -5,10 +5,47 @@
 
 const express = require('express'),
       app = express(),
+      fs = require('fs'),
       http = require('http').Server(app),
+      morgan = require('morgan');
       bodyParser = require('body-parser'),
-      users = require('./users.js')('stub');
+      sql = require('mysql');
 
+const config = JSON.parse(fs.readFileSync('config.json', "utf8"));
+
+const con = sql.createConnection({
+  host     : config.SQL_HOST,
+  user     : config.SQL_USER_TEST,
+  password : config.SQL_PASSWORD_TEST,
+  database : config.SQL_DB_NAME_TEST
+});
+
+// Users route, interface for all user DB interactions
+const users = require('./users.js')(con);
+
+app.use(morgan(config.LOG_LEVEL));
+// serve HTML from views directory
 app.use(express.static('views'));
+app.use(bodyParser.json());
+// all client calls to /user will go to users module
+app.use('/users', users); 
 
-http.listen(3020);
+con.connect(function(err){
+  if (err) throw err;
+  console.log("Connected to DB");
+});
+// Ugly but keeps the connection persistent by avoiding idle
+setInterval(function(){
+  con.query('SELECT 1');
+}, 10000);
+
+con.on('error', function(err){
+  console.log("Error is:", err);
+  throw err;
+});
+
+http.listen(
+  config.PORT, 
+  console.log("Server started, accepting requests on port:", 
+  config.PORT)
+);
--- authenticator/users.js
+++ authenticator/users.js
@@ -1,9 +1,13 @@
 /**
  * Module that provides an interface to our SQL DB, which keeps track of user
- * state
+ * state and an API for clients 
+ *
+ * Requires SQL connection object to be passed in
  */
 
-module.exports = (function(sql) {
+module.exports = (function(con) {
+  const router = require('express').Router(),
+  moment = require('moment');
 
   /*
    * Gets user's DB record by kerberos
@@ -23,4 +27,112 @@ module.exports = (function(sql) {
    */
   function getUserNameByUUID(uuid) {}
 
+  /*
+   * Insert a record into database
+   */
+  function insertRecord(kerberos, uuid, req, res){
+    con.query(
+      'INSERT INTO `minecraft_users` SET kerberos= ?, uuid = ?, time = ?',
+      [kerberos, uuid, '0'],
+      function(err, results, field) {
+        // TODO think about whether this is correct err handling behavior
+        if (err) {
+          if (err.code === 'ER_DUP_ENTRY') {
+            console.log("UUID is already in DB");
+          } else {
+            console.log(err);
+            throw err;
+          }
+          res.sendStatus(400);
+        } else { 
+          console.log('Successfully added record for', kerberos);
+        }
+      }
+    );
+  }
+
+  /*
+   * Update a record in the database
+   *
+   * Assumes that only one record exists in the DB
+   */
+  function updateRecord(kerberos, uuid, req, res){
+    con.query(
+      'UPDATE `minecraft_users` SET uuid = ?, time = ? WHERE kerberos = ?',
+      [uuid, '0', kerberos],
+      function(err, results, field) {
+        // TODO think about whether this is correct err handling behavior
+        if (err) {
+          if (err.code === 'ER_DUP_ENTRY') {
+            console.log("UUID is already in DB");
+          } else {
+            console.log(err);
+            throw err;
+          }
+          res.sendStatus(400);
+        } else { 
+          console.log('Successfully updated record for', kerberos);
+          res.sendStatus(200);
+        }
+      }
+    );
+  }
+
+  /*
+   * Route for clients to GET their UUID
+   */
+  router.post('/find_record', function(req, res){
+    let kerberos = req.body.kerberos;
+    console.log("Finding record for", kerberos);
+    con.query(
+      'SELECT uuid, time FROM  `minecraft_users` WHERE kerberos = ?',
+      kerberos, 
+      function(err, results, fields) {
+        if(err) {
+        } else {
+          if (results.length) {
+            //send record back in json
+            console.log(JSON.stringify(results[0]));
+            res.sendStatus(200);
+          } else {
+            // no result
+            res.sendStatus(404);
+          }
+        }
+
+      }
+    );
+  });
+
+  /*
+   * Route for clients to POST permission requests
+   */
+  router.post('/add_record', function(req, res){
+    let kerberos = req.body.kerberos,
+        uuid = req.body.uuid;
+    //TODO fetch UUID
+    console.log(req.body);
+    //TODO sanitize user input
+    con.query(
+      'SELECT 1 FROM  `minecraft_users` WHERE kerberos = ?',
+      kerberos,
+      function(err, results, field) {
+        // TODO handle error
+        if(err) {
+        }
+        console.log(results);
+        if (results.length) {
+          // Update
+          console.log("Updating record for:", kerberos);
+          updateRecord(kerberos, uuid, req, res);
+        } else {
+          // Insert
+          console.log("Adding record for:", kerberos);
+          insertRecord(kerberos, uuid, req, res);
+        }
+      }
+    );
+  });
+
+  return router;
 });
